3e15c7f2fa2fe859e7b447d58b487949a12dc47f
Knaeggel <nschwen1@stud.hs-offenburg.de>
2022-05-17 12:09:32 +0200

    PortA RA4 working

--- src/code/Timer.java
+++ src/code/Timer.java
@@ -1,6 +1,10 @@
 package code;
 
 public class Timer {
+    public static int getTimerIncrementCount() {
+        return timerIncrementCount;
+    }
+
     public static int timerIncrementCount = 0;
     public static int initPrescalerVal = 0;
     public static boolean disableTimer = false;
@@ -37,6 +41,30 @@ public class Timer {
                 timerInterrupt = true;
                 Decoder.obj.ram.incrementTMR0();
             }
+        }
+    }
+
+    public void incrementTimer0WithRa4(int prescalerValue){
+        if (initPrescalerVal != prescalerValue) {
+            Decoder.obj.ram.setTMR0(0);
+            initPrescalerVal = prescalerValue;
+            timerIncrementCount = prescalerValue;
+        }
+        timerIncrementCount--;
+
+        if (timerIncrementCount == 0) {
+            Decoder.obj.ram.incrementTMR0();
+            timerIncrementCount = initPrescalerVal;
+
+        } else if (timerIncrementCount < 0) {
+            int ilocal = timerIncrementCount;
+            if (ilocal < 0) {
+                ilocal *= (-1);
+            }
+            timerIncrementCount = initPrescalerVal;
+            timerIncrementCount -= ilocal;
+
+            Decoder.obj.ram.incrementTMR0();
         }
     }
 
